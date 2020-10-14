3ae55d74729d1daeabf1c53b2987294c718d7097
Arthur luz <arthurg.luz@gmail.com>
2020-10-14 12:16:24 -0300

    initial commit

---
+++ config/spotify/Users/22c67iltpl4zvvs4jshgymw3y-user/frecency.pb
@@ -0,0 +1,9 @@
+
+<
+%spotify:artist:5me0Irg2ANcsgc93uaYrpb	Ž¸²yw@ ¤ÖŸô
+;
+$spotify:album:0lCrpiIucZHq4pSmmXXrGO	KZ‡¨“w@ †ÅÈ÷
+;
+$spotify:album:2hjabdW8LVe9RdIYJLswub	jÀ’Ÿw@ ››Ñ÷
+O
+8spotify:user:janene64-nz:playlist:0RNH1U3gJwxChUbATZ9oXd	þŒ¥¹w@' ±òÓ÷
---
+++ doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" default))
+ '(package-selected-packages '(python-black blacken)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'projectile-grep 'disabled nil)
